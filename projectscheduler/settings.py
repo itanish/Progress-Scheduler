ca5169ca22fdf4077858fd43bdee779e2ef5a19c
itanish <tanishsehgal1234@gmail.com>
2020-07-20 03:48:53 +0530

    Final Deploy

--- projectscheduler/settings.py
+++ projectscheduler/settings.py
@@ -140,7 +140,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static/' 
+STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'static'), ] 
+STATIC_ROOT = os.path.join(BASE_DIR, 'collectstatic') 
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
