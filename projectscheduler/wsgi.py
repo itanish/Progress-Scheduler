68a57ce48bf95049c9e0a8c9da36ba1a1241e8a9
itanish <tanishsehgal1234@gmail.com>
2020-07-20 03:42:11 +0530

    Final Deploy

--- projectscheduler/wsgi.py
+++ projectscheduler/wsgi.py
@@ -7,21 +7,21 @@ For more information on this file, see
 https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
-import os
-
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projectscheduler.settings')
-
-application = get_wsgi_application()
-
 # import os
 
 # from django.core.wsgi import get_wsgi_application
-# from whitenoise.django import DjangoWhiteNoise
-
 
 # os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projectscheduler.settings')
 
 # application = get_wsgi_application()
-# application = DjangoWhiteNoise(application)
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
+
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projectscheduler.settings')
+
+application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
