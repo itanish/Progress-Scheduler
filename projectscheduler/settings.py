b35b86742bed71dc11802f4969aca8a6ee1384df
itanish <tanishsehgal1234@gmail.com>
2020-07-20 18:50:41 +0530

    Favicon fix

--- projectscheduler/settings.py
+++ projectscheduler/settings.py
@@ -86,16 +86,14 @@ TEMPLATES = [
 WSGI_APPLICATION = 'projectscheduler.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -106,7 +104,7 @@ DATABASES = {
         'PORT': '',
     }
 }
-
+'''
 
 
 # Password validation
